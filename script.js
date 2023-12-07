b85bfb8ae4eb3a8b50b3870628928864ba526fe6
Abilash <90945323+AbilashMugunth@users.noreply.github.com>
2023-12-07 21:27:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your JS code here. If required.
+let inputTitle = document.querySelector("#title");
+let inputAuthor = document.querySelector("#author");
+let inputISBN = document.querySelector("#isbn");
+let addBookBtn = document.querySelector("#submit");
+let tbody = document.querySelector("#book-list");
+
+addBookBtn.addEventListener("click", () => {
+  let tr = document.createElement("tr");
+
+  let titleCell = document.createElement("td");
+  titleCell.innerText = inputTitle.value;
+
+  let authorCell = document.createElement("td");
+  authorCell.innerText = inputAuthor.value;
+
+  let isbnCell = document.createElement("td");
+  isbnCell.innerText = inputISBN.value;
+
+  let delBtn = document.createElement("button");
+  delBtn.innerText = "X";
+  delBtn.className = "delete-btn";
+
+  tr.append(titleCell, authorCell, isbnCell, delBtn);
+  tbody.appendChild(tr);
+
+  delBtn.addEventListener("click", () => {
+    delBtn.parentElement.remove();
+  });
+});
